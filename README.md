bab8f1c20408fb318dbbf9203768bab3240012b4
Roberto Pineda <rpinedan30@gmail.com>
2025-01-04 18:50:55 +0700

    project created

---
+++ README.md
@@ -0,0 +1,9 @@
+Readictionary for Android
+
+A language learning tool for avid readers who want to learn a language in the fastest way possible, just read!
+Instant live translation using the DeepSeek V3 LLM.
+
+1. Upload PDF, ePUB, plain text or other reader formats and start reading them with a live dictionary.
+2. No need to look up each word, the app instantly tells you the definition of the word while you are reading.
+3. The more you read, the faster you learn words.
+4. Since it works with a LLM, you can translate to any language.
---
+++ app/src/androidTest/java/com/robertopineda/android_readictionary/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.robertopineda.android_readictionary
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.robertopineda.android_readictionary", appContext.packageName)
+    }
+}
